0501b43989c9dd784972a308afe96d55aaf2cbe2
seif-maachaoui <seifeddine.maachaoui@laplateforme.io>
2023-03-18 10:02:26 +0100

    J'apporte des modifications à ma première fonction, j'ajoute un paramètre stop_second, afin de faire en sorte qu'au bout de 10 secondes, le temps s'arrête et affiche le temps après 10 secondes. J'en profite aussi pour ajouter quelques commentaires

--- horloge.py
+++ horloge.py
@@ -1,8 +1,8 @@
 # Importation des modules
 import time
 
-# Je commence par définir ma fonction qui prend en paramètre l'heure, les minutes et les secondes
-def horloge(hh, mm, ss):
+# Je commence par définir ma fonction qui prend en paramètre l'heure, les minutes, les secondes et stop_second
+def horloge(hh, mm, ss, stop_second):
     
     #Je place une boucle while qui permet d'afficher l'heure, les minutes et les secondes en continue
     while True:
@@ -11,10 +11,10 @@ def horloge(hh, mm, ss):
         #end=\r pour que l'heure reste sur la même ligne
         #flush=true pour forcer l'affichage de la valeur sans attendre le remplissage de la mémoire tampon
         
-        print(f"{hh:02}:{mm:02}:{ss:02}", end="\r", flush=True)
-
+        print("Heure non modifiable : ", f"{hh:02}:{mm:02}:{ss:02}", end="\r", flush=True)
+        
         #J'incrémente de +1 ma variable de seconde avant tout
-        ss += 1 
+        ss += 1
         if ss == 60:        #Si on atteint 60 secondes, alors...
             mm += 1         #Ajoute + 1 aux minutes
             ss = 0          #Et je réinitialise mes secondes à 0     
@@ -25,5 +25,17 @@ def horloge(hh, mm, ss):
             hh = 0          #On réinitialise la valeur d'heure à 0
         
         # J'attends une seconde avant de rafraîchir l'heure affichée grâce à la fonction sleep
-        time.sleep(1)                       
-horloge(16, 30, 00)   #J'appelle ma fonction horloge en lui passant comme paramètre 16, 30, 00
+        time.sleep(1)           
+
+        #Je veux à présent que dès que les secondes atteignent stop_second, on sort de la boucle
+        if ss == stop_second:
+            break
+    #Je n'oublie pas de retourner l'heure
+    return hh, mm, ss   
+
+#Je stocke ma valeur retournée dans une variable temps
+# Qui appellera ma fonction horloge, qui est un tuple avec pour paramètre hh, mm, ss et stop_second
+temps = horloge(16, 30, 00, 10)
+#Puis j'affiche le temps après 10 secondes
+print("Le temps après 10 secondes : ", f"{temps[0]:02d}:{temps[1]:02d}:{temps[2]:02d}")
+
